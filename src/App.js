841215ea54f92e1f2a0cc10f3ca1a02b0bff315b
Jonathan Aguilar <jonathan.aguilar@revolveclothing.com>
2018-10-18 16:08:26 -0700

    dd line-through, add remove, and all done

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ class App extends Component {
   formSubmitted(event) {
     event.preventDefault();
 
-    console.log(this.state.newTodo);
     this.setState({
       newTodo: '',
       todos: [...this.state.todos, {
@@ -29,6 +28,40 @@ class App extends Component {
       }]
     });
   }
+
+  toggleTodoDone(event, index) {
+    console.log(event.target.checked)
+    const todos = [...this.state.todos];// copy the array
+    todos[index] = {...todos[index]}; //
+    todos[index].done = event.target.checked;
+    this.setState({
+      todos
+    });
+  }
+
+  removeTodo(index) {
+    const todos = [...this.state.todos]// copy the array
+    todos.splice(index, 1);
+
+    this.setState({
+      todos
+    });
+  }
+
+  allDone() {
+    const todos = this.state.todos.map(todo => {
+      return {
+        title: todo.title, // can also do ...todo
+        done: true
+      };
+    });
+    this.setState({
+      todos
+    });
+  }
+
+
+
   render() {
     return (
       <div className="App">
@@ -38,9 +71,14 @@ class App extends Component {
         <input id="newTodo" onChange={(event) => this.newTodoChanged(event)} name="newTodo" value={this.state.newTodo}/>
         <button type="submit">Add Todo</button>
         </form>
+        <button onClick={() => this.allDone()}>All Done</button>
         <ul>
-          {this.state.todos.map(todo => {
-            return <li key={todo.title}>{todo.title}</li>
+          {this.state.todos.map((todo, index) => {
+            return(<li key={todo.title}>
+              <input onChange={(event) => this.toggleTodoDone(event, index)} type="checkbox" checked={todo.done}/>
+              <span className={todo.done ? 'done' : ''}>{todo.title}</span>
+              <button onClick={() => this.removeTodo(index)}>Remove</button>
+            </li>)
           })}
         </ul>
       </div>
